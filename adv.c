af21d93a7eedce753d264a8137a01a5b216cc1e0
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-01-22 21:02:43 -0300

    .

--- adv.c
+++ adv.c
@@ -1,9 +1,31 @@
-#include <stidio.h> // entrada e saida padrao
+#include <stdio.h> // entrada e saida padrao
 
-int main( ) { 
+int main( ) {
 
-printf("************************************");
-printf("* Bem-vindo ao Jogo de Adivinhação *");
-printf("************************************");
+printf("************************************\n");
+printf("* Bem-vindo ao Jogo de Advinhacao *\n");
+printf("************************************\n");
+
+int numerosecreto;
+numerosecreto = 42;
+
+int chute; //int = numeiro inteiro
+
+printf ("Qual eh o seu chute? ");
+scanf("%d", &chute); //%d armazenar e ler um numero inteiro
+printf("Seu chute foi %d \n", chute);
+
+//
+
+if (chute == numerosecreto) {
+    printf ("Parabens! Voce acertou!\n");
+} else {
+    if (chute > numerosecreto) {
+        printf("Seu chute foi maior do que o numero secreto!\n");
+        }
+    if (chute < numerosecreto) {
+        printf("Seu chute foi menor do que o numero secreto!\n");
+    }
+}
 
 }
