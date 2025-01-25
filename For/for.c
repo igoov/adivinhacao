fd8ebbb6a3fd3ec20c424ff4ad78f3b59a5fd1e8
Liedsonfsa <fabricoliedson@gmail.com>
2025-01-25 09:28:49 -0300

    utilizando a função rand para gerar um número aleatório

--- For/for.c
+++ For/for.c
@@ -1,14 +1,19 @@
 #include <stdio.h> // entrada e saida padrao
+#include <stdlib.h>
+#include <time.h>
+
 #define NUMERO_DE_TENTATIVAS 3 //numeros magicos sao numeros importantes para o nosso progama 
 
 int main()
 {
 
+    srand(time(NULL));
     printf("***********************************\n");
     printf("* Bem-vindo ao Jogo de Advinhacao *\n");
     printf("***********************************\n");
 
-    int numerosecreto = 42;
+    int numerosecreto = (rand() % 100) + 1;
+    printf("rand: %d\n", numerosecreto);
     int chute; // int = numeiro inteiro
 
 // variavel i com o numero 1, condição de parada i <= 3, i++
@@ -23,36 +28,34 @@ int main()
 //i ++ começa com 1 e vai somando a cada tentativa nesse cenario.
 //O i++ adiciona 1 e o i-- subtrai 1 do valor atual da variável.
 
-for (int i = 1; i < 3; i++) { 
+    for (int i = 1; i <= 3; i++) {
 
-    printf("Qual eh o seu chute?", i);
-    scanf("%d", &chute);
+        printf("Qual eh o seu chute? ");
+        scanf("%d", &chute);
 
-    if (chute < 0 ) {
-        printf("Voce nao pode chutar numeros negativos\n");
-        i--; //devolve uma chance pro jogador, 
-        continue; //diferente do break, ele pulara para a proxima interação
-    }
-    
-    printf("Seu %do. chute foi %d\n",i, chute);
-
-    int acertou = chute == numerosecreto;
-    int maior = chute > numerosecreto;
-    int menor = chute < numerosecreto;
-
-    if (acertou) {
-        printf("Parabens! Voce acertou!\n");
-        break; //break apos acertar 
-    } else {
-        int  maior = chute > numerosecreto;
-        if (maior) {
-            printf("Seu chute foi maior do que o numero secreto!\n");
+        if (chute < 0 ) {
+            printf("Voce nao pode chutar numeros negativos\n");
+            i--; //devolve uma chance pro jogador, 
+            continue; //diferente do break, ele pulara para a proxima interação
         }
-        else {
-            printf("Seu chute foi maior do que o numero secreto!\n");
+        
+        printf("Seu %dº chute foi %d\n",i, chute);
+
+        int acertou = chute == numerosecreto;
+        int maior = chute > numerosecreto;
+        int menor = chute < numerosecreto;
+        
+        if (acertou) {
+            printf("Parabens! Voce acertou!\n");
+            break; //break apos acertar 
+        } else {
+            if (maior) {
+                printf("Seu chute foi maior do que o numero secreto!\n");
+            } else {
+                printf("Seu chute foi menor do que o numero secreto!\n");
             }
         }
     }
 
-printf("Obrigado por jogar!\n");
+    printf("Obrigado por jogar!\n");
 }
