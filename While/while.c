cce176be0ff5a2a0bc72a54e6383e68709c0b348
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-01-25 03:27:48 -0300

    .

--- While/while.c
+++ While/while.c
@@ -13,19 +13,18 @@ int main()
     int ganhou = 0;
     int tentativas = 1;
 
-    while (!ganhou) {
+    while (1) {
 
 
-    printf("Qual eh o seu chute?", i);
+    printf("Qual eh o seu chute?", tentativas);
     scanf("%d", &chute);
 
     if (chute < 0 ) {
-        printf("Voce nao pode chutar numeros negativos\n");
-        i--; //devolve uma chance pro jogador, 
+        printf("Voce nao pode chutar numeros negativos\n"); 
         continue; //diferente do break, ele pulara para a proxima interação
     }
     
-    printf("Seu %do. chute foi %d\n",i, chute);
+    printf("Seu %do. chute foi %d\n",tentativas, chute);
 
     int acertou = chute == numerosecreto;
     int maior = chute > numerosecreto;
@@ -33,7 +32,7 @@ int main()
 
     if (acertou) {
         printf("Parabens! Voce acertou!\n");
-        break; //break apos acertar 
+        break; 
     } else {
         int  maior = chute > numerosecreto;
         if (maior) {
@@ -43,6 +42,7 @@ int main()
             printf("Seu chute foi maior do que o numero secreto!\n");
             }
         }
+        tentativas++; //somar as tentavas
     }
 
 printf("Obrigado por jogar!\n");
--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
  *** Projeto para estudo da linguagem C ***
  
+ *** Sabe a quantidade? for.
+ Não sabe? while. ***
+
+4.9 Resumindo
+
+ • A tomar decisões em nossos programas,usando ifs;
+ • A tomar muitas decisões,usando ifs, else ifS e elseS;
+ • A fazer loops contados como for;
+ • A fazer loops com while;
+ • A criar constantesusando #define.
