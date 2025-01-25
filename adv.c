0af16a8f62bc9ca0825b6c3bd6a1cda3a9e424c7
MDchefin_ <123131848+igoov@users.noreply.github.com>
2025-01-25 02:42:34 -0300

    .

--- adv.c
+++ adv.c
@@ -1,29 +1,60 @@
 #include <stdio.h> // entrada e saida padrao
+#define NUMERO_DE_TENTATIVAS 3 //numeros magicos sao numeros importantes para o nosso progama 
 
-int main( ) {
+int main()
+{
 
-printf("************************************\n");
-printf("* Bem-vindo ao Jogo de Advinhacao *\n");
-printf("************************************\n");
+    printf("************************************\n");
+    printf("* Bem-vindo ao Jogo de Advinhacao *\n");
+    printf("************************************\n");
 
-int numerosecreto;
-numerosecreto = 42;
+    int numerosecreto;
+    numerosecreto = 42;
 
-int chute; //int = numeiro inteiro
+    int chute; // int = numeiro inteiro
 
-printf ("Qual eh o seu chute? ");
-scanf("%d", &chute); //%d armazenar e ler um numero inteiro
-printf("Seu chute foi %d \n", chute);
+// variavel i com o numero 1, condição de parada i <= 3, i++
+//soma 1 a variavel = 0,1,2 e quando for igual a 3, a condicao sera falsa e o loop parara.
 
-if (chute == numerosecreto) {
-    printf ("Parabens! Voce acertou!\n");
-} else {
-    if (chute > numerosecreto) {
-        printf("Seu chute foi maior do que o numero secreto!\n");
-        }
-    if (chute < numerosecreto) {
-        printf("Seu chute foi menor do que o numero secreto!\n");
+//O primeiro bloco, o de inici
+//alização, é executado uma única vez no começo. Em seguida, ele executa o
+//segundo bloco, a condição. Se ela for verdadeira, ele dispara o bloco de có
+//digo. Por fim, ele executa o terceiro bloco, o incremento. E aí repete, valida a
+//condição, executabloco, executaincremento, assimpordiante, atéacondição
+//deixar de ser verdadeira
+//i ++ começa com 1 e vai somando a cada tentativa nesse cenario.
+//O i++ adiciona 1 e o i-- subtrai 1 do valor atual da variável.
+
+for (int i = 1; i < 3; i++) { 
+
+    printf("Qual eh o seu chute?", i);
+    scanf("%d", &chute);
+
+    if (chute < 0 ) {
+        printf("Voce nao pode chutar numeros negativos\n");
+        i--; //devolve uma chance pro jogador, 
+        continue; //diferente do break, ele pulara para a proxima interação
     }
-}
+    
+    printf("Seu %do. chute foi %d\n",i, chute);
 
+    int acertou = chute == numerosecreto;
+    int maior = chute > numerosecreto;
+    int menor = chute < numerosecreto;
+
+    if (acertou) {
+        printf("Parabens! Voce acertou!\n");
+        break; //break apos acertar 
+    } else {
+        int  maior = chute > numerosecreto;
+        if (maior) {
+            printf("Seu chute foi maior do que o numero secreto!\n");
+        }
+        else {
+            printf("Seu chute foi maior do que o numero secreto!\n");
+            }
+        }
+    }
+
+printf("Obrigado por jogar!\n");
 }
